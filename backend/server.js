75a00ec08c4010db18de2835ef261e3879d92670
Pratyush Goutam <pratg5935@gmail.com>
2025-07-02 20:44:24 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ 
-    status: 'OK', 
-    timestamp: new Date().toISOString() 
-  });
-});
-
 // Connect to Database
 
 connectDB()
